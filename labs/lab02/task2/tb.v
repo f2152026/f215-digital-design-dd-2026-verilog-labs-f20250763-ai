2c30808de461a0623d9618763e75bbba4f50dd35
Aarav Bhardwaj <f20250763@pilani.bits-pilani.ac.in>
2026-09-15 05:19:45 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -4,7 +4,13 @@
 module tb;
 
   // TODO: declare the inputs and outputs
+reg [1:0] sel;
+wire [7:0] dout;
 
+lut U1 (
+    .sel(sel),
+    .dout(dout)
+);
   // TODO: instantiate DUT here
 
   // Waveform dump configuration (DO NOT CHANGE)
@@ -12,16 +18,19 @@ module tb;
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
       $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+      $dumpvars(0, U1);
     end
   end
 
   initial begin
-    // TODO: apply different input combinations
-
-  end
+    sel = 0;
+    #5 sel = 1;
+    #5 sel = 2;
+    #5 sel = 3;
+    #5 $finish;
+end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%b | dout=%b", sel, dout); // change as required
 
 endmodule
